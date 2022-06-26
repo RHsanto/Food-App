d78ffdb31a6cc2d9471ad32600d61d970e660b96
RHsanto <rakibulsanto789@gmail.com>
2022-06-27 01:28:24 +0600

    add categories Data

---
+++ src/data/categories.data.js
@@ -0,0 +1,22 @@
+export const Categories_Data = [
+{
+  id:1,
+  image: require('../../assets/images/pizza-icon.png'),
+  title:'Pizza',
+  selected:true,
+},
+{
+  id:2,
+  image: require('../../assets/images/shrimp-icon.png'),
+  title:'Seafood',
+  selected:true,
+},
+{
+  id:3,
+  image: require('../../assets/images/soda-icon.png'),
+  title:'Soft Drinks',
+  selected:true,
+},
+
+
+]
