f10ba58c32e8c9adcf3fd2d6d660c56ab61bedf4
RHsanto <rakibulsanto789@gmail.com>
2022-06-26 20:24:10 +0600

    add text components

---
+++ src/screens/Details.js
@@ -0,0 +1,12 @@
+
+import { View } from 'react-native';
+import Text from '../components/Text/Text';
+
+export default function  Details(){
+  return (
+    <View>
+      <Text>Hello Details</Text>
+    </View>
+  );
+};
+
---
+++ src/screens/Home.js
@@ -0,0 +1,17 @@
+
+import { View } from "react-native";
+import Text from "../components/Text/Text";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Hello My Home</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+
+
+
